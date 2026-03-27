d4ddf738d9f657de803f2d064b7a0ad920c66bda
@ <157278895+yerdaulettt@users.noreply.github.com>
2026-03-27 09:39:19 +0500

    task 2 solution

---
+++ p6/task2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func withMutex() {
+	var counter int
+	var wg sync.WaitGroup
+	var m sync.Mutex
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			m.Lock()
+			counter++
+			m.Unlock()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Counter using mutex:", counter)
+}
+
+func withAtomic() {
+	var counter atomic.Int32
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter.Add(1)
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Counter using atomic:", counter.Load())
+}
+
+func main() {
+	withMutex()
+	withAtomic()
+}
