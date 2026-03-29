fdd9e11357ce7ef7350e4d69fcea8400baffe496
@ <157278895+yerdaulettt@users.noreply.github.com>
2026-03-29 16:51:40 +0500

    task 3 solution

---
+++ p6/task3/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func startServer(ctx context.Context, name string) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		defer close(out)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(time.Duration(rand.Intn(500)) * time.Millisecond):
+				out <- fmt.Sprintf("[%s] metric: %d", name, rand.Intn(100))
+			}
+		}
+	}()
+
+	return out
+}
+
+func FanIn(ctx context.Context, ctotal ...<-chan string) <-chan string {
+	result := make(chan string)
+	var wg sync.WaitGroup
+
+	for _, c := range ctotal {
+		wg.Add(1)
+		go func(c <-chan string) {
+			defer wg.Done()
+			for v := range c {
+				result <- v
+			}
+		}(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
+
+	return result
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	ch1 := startServer(ctx, "Alpha")
+	ch2 := startServer(ctx, "Beta")
+	ch3 := startServer(ctx, "Gamma")
+	ch4 := FanIn(ctx, ch1, ch2, ch3)
+
+	for val := range ch4 {
+		fmt.Println(val)
+	}
+}
